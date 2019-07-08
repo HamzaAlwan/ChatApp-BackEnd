96c9e1ec6aa3662063ce393704146232cfef2d95
HamzaAlwan <ihamzaalwan@gmail.com>
2019-07-08 10:41:15 +0300

    update database url

--- config/secret.js
+++ config/secret.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MongoUrl: 'mongodb://localhost/chatapp',
-    secret: 'jwtSecret'
+	MongoUrl: "mongodb://admin:rbk1234@ds217976.mlab.com:17976/chat-app",
+	secret: "jwtSecret"
 };
